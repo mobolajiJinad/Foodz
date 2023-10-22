60c52b86ca283657d11d4d5e053c76c6d032cce0
Abd'Quadri Jinad <abdjinadquadri@gmail.com>
2023-10-22 03:44:21 +0100

    Made website more responseive, deleted settings.json file

--- js/script.js
+++ js/script.js
@@ -2,21 +2,34 @@
 const cards = document.querySelectorAll(".card");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
+const hamburgerIcon = document.querySelector(".hamburgerMenu").children[0];
+
+hamburgerIcon.addEventListener("click", () => {
+  console.log(hamburgerIcon.src);
+  if (hamburgerIcon.src.includes("hamburger.svg")) {
+    hamburgerIcon.src = "/images/hamburgerOpen.svg";
+  } else {
+    hamburgerIcon.src = "/images/hamburger.svg";
+    console.log("dasjdjks");
+  }
+
+  document.querySelector(".menu").classList.toggle("menu-mobile");
+});
 
 const cardWidth = cards[0].clientWidth + 24;
 let currentIndex = 0; // Start with the middle card
 
 const update = () => {
-  const translateValue = -currentIndex * cardWidth;
+  const translateValue = -currentIndex * (cardWidth + 10);
   cardContainer.style.transform = `translateX(${translateValue}px)`;
 
   cards.forEach((card, index) => {
     if (index === currentIndex) {
       card.style.transform = "scale(1.05)";
-      card.style.width = "calc(30% + 48px)"; // Expand the middle card
+      card.style.width = `calc(${card.innerWidth} + 48px)`; // Expand the middle card
     } else {
       card.style.transform = "scale(1)";
-      card.style.width = "30%"; // Reset width for other cards
+      card.style.width = `${card.innerWidth}`; // Reset width for other cards
     }
   });
 };
